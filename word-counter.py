8b6f6cc49b758548aaf2ec6ac24bf8b83ff13753
Asiri Pramodaya <asiripramoday2000@gmail.com>
2024-05-11 08:27:50 +0530

    added some modifications

--- word-counter.py
+++ word-counter.py
@@ -1,4 +1,4 @@
-print("Enter a sentence, I will show the word count")
-sentence = input("Type here: ")
-myList = sentence.split() # splits the string into a list
+print("Enter a text, I will show the word count")
+text = input("Type here: ")
+myList = text.split() # splits the string into a list
 print("There ",len(myList)," words in your sentence.")
